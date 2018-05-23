b31bd9adbf3d1c7a50a929d5be3284b01f5d2d5a
sunzn <sunzn.cn@gmail.com>
2018-05-23 16:19:45 +0800

    Update Sample

--- Sample/src/main/java/com/sunzn/divider/sample/MainActivity.java
+++ Sample/src/main/java/com/sunzn/divider/sample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sunzn.divider.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.sunzn.divider.library.HorizontalDivider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = findViewById(R.id.rv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new HorizontalDivider(this, R.drawable.shape_item_line, 10, 10, false));
+        mRecyclerView.setAdapter(new RecyclerAdapter());
     }
 }
---
+++ Sample/src/main/java/com/sunzn/divider/sample/RecyclerAdapter.java
@@ -0,0 +1,43 @@
+package com.sunzn.divider.sample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.getTextView().setText(String.format(Locale.getDefault(), "position %d", position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item_view_text);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+}
